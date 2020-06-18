b82539c4cddcc3ca85dd3ffd225481fcb83a2192
Sathyavathi <sathyavathi14@gmail.com>
2020-06-18 23:31:15 +1000

    Function

---
+++ sample-js/day3/src/functionsdemo.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//function which accepts parameters and doesnot return a value//
+
+//string introplotion =`Hello, ${name}`//
+
+const sayHello = function(name){
+    console.log(`Hello, ${name}`);
+}
+
+sayHello("satya");
+
+sayHello('rahul');
+
+sayHello(`hareesh`);
+
